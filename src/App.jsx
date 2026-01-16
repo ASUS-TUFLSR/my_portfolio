ba4f051c612e5e440ca7876a76c286b08ede96fd
Manmit Pal <manmitpal348@gmail.com>
2026-01-16 13:18:51 +0530

    setting up the environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-4xl' >App</div>
+    <>
+      <BrowserRouter>
+       
+      </BrowserRouter>
+    </>
   )
 }
 
