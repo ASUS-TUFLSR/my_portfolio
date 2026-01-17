af4aad2af8cb4fb1ffa582f87c1ae7dc315438db
Manmit Pal <manmitpal348@gmail.com>
2026-01-17 15:20:35 +0530

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import "./index.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-       
+         <Routes>
+          <Route index element={<Home/>} />
+          <Route path="*" element={<NotFound/>} />
+         </Routes>
       </BrowserRouter>
     </>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen' >
+        {/* theme toggle */}
+
+        {/* Background effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
